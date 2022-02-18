904ecaa5a7906aed90293fa1147892c0f2628db0
fcolmenar <ferchyno@hotmail.com>
2022-02-18 20:49:00 +0100

    Supply Chain implementation in progress

--- project-6/contracts/coffeecore/Ownable.sol
+++ project-6/contracts/coffeecore/Ownable.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.24;
 
+import "../coffeebase/SupplyChain.sol";
+
 /// Provides basic authorization control
 contract Ownable {
     address private origOwner;
--- project-6/truffle.js
+++ project-6/truffle.js
@@ -4,6 +4,38 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    develop: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/f85ca96666304af18d77aa32afaf5293`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    timeout: 100000
+ },
+
+    // Configure your compilers
+    compilers: {
+      solc: {
+        // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+        version: "0.4.24",      // Fetch exact version from solc-bin (default: truffle's version)
+        // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+        // settings: {          // See the solidity docs for advice about optimization and evmVersion
+        //  optimizer: {
+        //    enabled: false,
+        //    runs: 200
+        //  },
+        //  evmVersion: "byzantium"
+        // }
+      }
     }
-  }
 };
